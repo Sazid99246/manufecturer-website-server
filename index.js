d333e6152f5c835f5bb20361e165f46d64c689b4
Sheikh MD Sazidul Islam <sheikhmdsazidulislam@gmail.com>
2022-05-23 12:22:25 +0600

    intial setup completed

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const port = process.env.PORT || 5000;
+const app = express();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+// middleware
+app.use(cors())
+app.use(express.json())
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.gau9b.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+app.get('/', (req, res)=>{
+    res.send("Hello from rainbow computers server")
+})
+app.listen(port, ()=>{
+    console.log("Rainbow computers is listening from port", port);
+})
